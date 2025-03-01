883123bf278de31f47dc8db4b0022ae239185d07
Preston Thomas <prestondthomas72@gmail.com>
2025-02-28 21:15:56 -0700

    changed git klint-ci.yml

--- shared/src/commonTest/kotlin/com/example/cipipeline/Test.kt
+++ shared/src/commonTest/kotlin/com/example/cipipeline/Test.kt
@@ -4,6 +4,7 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class CommonGreetingTest {
+    // Simple test but...
     @Test
     fun testExample() {
         assertTrue(Greeting().greet().contains("Hello"), "Check 'Hello' is mentioned")
